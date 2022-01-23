5a21a744e489e4d6051759675fdb540ef24935ad
Fedeperrone98 <f.perrone6@studenti.unipi.it>
2022-01-23 20:02:34 +0100

    add button deleteRecipe and updateRecipe

--- CooGether/coogetherApp/src/main/java/it/unipi/lmmsdb/coogether/coogetherapp/controller/UpdateRecipeViewController.java
+++ CooGether/coogetherApp/src/main/java/it/unipi/lmmsdb/coogether/coogetherapp/controller/UpdateRecipeViewController.java
@@ -1,4 +1,29 @@
 package it.unipi.lmmsdb.coogether.coogetherapp.controller;
 
+import it.unipi.lmmsdb.coogether.coogetherapp.bean.User;
+import it.unipi.lmmsdb.coogether.coogetherapp.config.SessionUtils;
+import it.unipi.lmmsdb.coogether.coogetherapp.utils.Utils;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+
 public class UpdateRecipeViewController {
+
+    @FXML
+    private void goBack(MouseEvent mouseEvent) {
+        ActionEvent ae = new ActionEvent(mouseEvent.getSource(), mouseEvent.getTarget());
+        Utils.changeScene("hello-view.fxml", ae);
+    }
+
+    @FXML
+    private void log(MouseEvent mouseEvent) {
+        //mostra i dati dello user se questo è loggato, altrimenti ad una pagina per fare il login
+        ActionEvent ae = new ActionEvent(mouseEvent.getSource(), mouseEvent.getTarget());
+        User logged = SessionUtils.getUserLogged();
+        if(logged==null){
+            Utils.changeScene("login-view.fxml", ae);
+        }else{
+            Utils.changeScene("user-details-view.fxml", ae);
+        }
+    }
 }
